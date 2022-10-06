59bd9d5fc63bd445c81d1f32c39a8758d5008f6d
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2022-10-06 18:25:45 -0400

    alpha-build

---
+++ alpha-build/main.js
@@ -0,0 +1,32 @@
+// import 'animate.css';
+'use strict';
+let horizon = document.getElementById('hzd');
+let hTextbox = document.getElementById('horizonTextbox');
+
+horizon.addEventListener('mouseover', function(){
+anime({
+    targets: '.hzdimage',
+    scale: (1.3),
+  });
+  anime({
+    targets: '#horizonTextbox',
+    scale: (1.3),
+    opacity: .8,
+    easing: 'easeOutQuad',
+    delay: '250',
+    duration: 200,
+  });
+})
+
+horizon.addEventListener('mouseout', function(){
+    anime({
+        targets: '.hzdimage',
+        scale: (1),
+      });
+      anime({
+        targets: '#horizonTextbox',
+        scale: (1),
+        opacity: 0,
+      });
+})
+
